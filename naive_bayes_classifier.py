03ce9fd1b63868902e42b1f9fe36b7fb5415b881
Bikash Das <dasbikash989@gmail.com>
2019-10-22 23:19:20 +0530

    Cleaned review text

---
+++ naive_bayes_classifier.py
@@ -0,0 +1,37 @@
+
+
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
+from collections import Counter
+import numpy as np 
+class naive_bayes_classifier:
+    def __init__(self):
+        self.positive_counts = Counter()
+        self.negative_counts = Counter()
+        self.total_counts = Counter()
+        self.pos_label_count = 0 # for calc prior p(pos)
+        self.neg_label_count = 0 # for calc prior p(neg)
+        
+    def clean_review_text(text):
+        '''
+            Returns the clean text after removing stop words
+            :params: uncleaned text review
+            :out_params: a filtered text review
+        '''
+        text = text.lower()
+        stop_words = set(stopwords.words('english'))
+        stop_words.add(',')
+        stop_words.add('.')
+        stop_words.add('br')
+        stop_words.add('”')
+
+        word_tokens = word_tokenize(text)
+
+        filtered_sentence = [w for w in word_tokens if not w in stop_words]
+        filtered_sentence = []
+        for w in word_tokens:
+            if w not in stop_words and w.find('\'') != 0:
+                filtered_sentence.append(w)
+        return filtered_sentence
+
+
