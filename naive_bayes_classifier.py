a50b334ee4425bc538fd38b0a03b44e8aeb7f161
Bikash Das <dasbikash989@gmail.com>
2019-10-23 23:57:22 +0530

    Restructured the code into different modules

--- naive_bayes_classifier.py
+++ naive_bayes_classifier.py
@@ -1,52 +1,117 @@
-
-
+# Refactoring Code
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize 
 from collections import Counter
-import numpy as np 
-import string
-class naive_bayes_classifier:
+import numpy as np
+class NB:
     def __init__(self):
+        # read reviews and labels
+        g = open('reviews.txt','r') #what we know
+        self.reviews = list(map(lambda x:x[:-1], g.readlines()))
+        g.close()
+        g = open('labels.txt','r') # what we want to know
+        self.labels = list(map(lambda x:x[:-1].upper(), g.readlines()))
+        g.close()
+        
+        assert(len(self.reviews) == len(self.labels))
+        
+        self.pos_cp = Counter()  
+        self.neg_cp = Counter()
+        self.prior_pos = 0
+        self.prior_neg = 0
+        self.vocab = []
+        self.count_vocab = 0
         self.positive_counts = Counter()
         self.negative_counts = Counter()
-        self.total_counts = Counter()
-        self.pos_label_count = 0 # for calc prior p(pos)
-        self.neg_label_count = 0 # for calc prior p(neg)
-
-    def clean_review_text(self,text):
+        self.count_positive_words = 0
+        self.count_negative_words = 0
+        self.pos_label_count = 0
+        self.neg_label_count = 0
+        
+        self.process(self.reviews[:24000], self.labels[:24000])
+        self.fit(self.reviews[24000:], self.labels[24000:])
+        
+        
+    
+    def clean(self,text):
         '''
             Returns the clean text after removing stop words
             :params: uncleaned text review
-            :out_params: a filtered text review
+            :out_params: cleaned review
         '''
         text = text.lower()
-        stop_words = set(stopwords.words('english'))
+        stop_words = set(stopwords.words('english')) 
         stop_words.add(',')
         stop_words.add('.')
         stop_words.add('br')
         stop_words.add('”')
 
-        word_tokens = word_tokenize(text)
+        word_tokens = word_tokenize(text) 
 
-        filtered_sentence = [w for w in word_tokens if not w in stop_words]
-        filtered_sentence = []
-        for w in word_tokens:
-            if w not in stop_words and w.find('\'') != 0:
-                filtered_sentence.append(w)
+        filtered_sentence = [w for w in word_tokens if not w in stop_words] 
+
+        filtered_sentence = [] 
+
+        for w in word_tokens: 
+            if w not in stop_words and w.find('\'') != 0: 
+                filtered_sentence.append(w) 
+
         return filtered_sentence
+    
+    def process(self,reviews,labels):
+        total_counts = Counter()
 
-    def counts(self,reviews,labels):
-        pass
-
-    def find_prior(self):
-        pass
+        for i in range(len(reviews)):
+            review = self.clean(reviews[i])  # clean the reviews first then process
+            if(labels[i] == 'POSITIVE'):
+                self.pos_label_count += 1
+                for word in review:
+                    self.positive_counts[word] += 1      # all positive words with their counts
+                    total_counts[word] += 1
+            else:
+                self.neg_label_count += 1
+                for word in review:
+                    self.negative_counts[word] += 1      # all negative words with their counts
+                    total_counts[word] += 1
+                    
+        self.vocab = list(total_counts.keys())  # vocabulary
+        self.count_vocab = len(self.vocab)           # total no of vocabulary
+        self.count_positive_words = len(self.positive_counts)
+        self.count_negative_words = len(self.negative_counts)
+        
+        # call train after process
+        self.train()
+    
+    def train(self):    
+  
+        # finding conditional probability of each word for each class
+        for word in self.vocab:
+            self.pos_cp[word] = np.log((self.positive_counts[word] + 1) / (self.count_positive_words + self.count_vocab))
+            self.neg_cp[word] = np.log((self.negative_counts[word] + 1) / (self.count_negative_words + self.count_vocab))
+        
+        # finding prior of each class
+        self.prior_pos = np.log(self.pos_label_count / len(self.labels))   # p(C==c) = Nc / N  where N = total labels/reviews
+        self.prior_neg = np.log(self.neg_label_count / len(self.labels))
     
     def test(self,review):
-        pass
+        review = self.clean(review)
+        test_pos = self.prior_pos
+        test_neg = self.prior_neg
+        for word in review:
+            if word in self.vocab:
+                test_pos += self.pos_cp[word]
+                test_neg += self.neg_cp[word]
+        return ('POSITIVE' if test_pos > test_neg else 'NEGATIVE') 
+    
+    def fit(self, test_set_reviews, test_set_labels):
+        correct = 0
+        for r,l in zip(test_set_reviews, test_set_labels):
+            test_value = self.test(r)
+            if(test_value == l):
+                correct += 1 
+        print("total correct: {} and incorrect: {}".format(correct,len(test_set_labels)-correct))
+        
+            
 
-
-# load review and label files
-
-
-
-
+        
+nb = NB()
