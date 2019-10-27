e90b4c9d6b78db2790e78ed6308fcf9a313cb84b
Bikash Das <dasbikash989@gmail.com>
2019-10-27 17:05:53 +0530

    loaded the pickle file and for review called it's test function

--- run.py
+++ run.py
@@ -2,12 +2,16 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, TextAreaField
 from wtforms.validators import DataRequired
+from naive_bayes_classifier import NB
+import pickle
 
-
-  
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1234252333' # some secret key
 
+# load the model.pickle  file 
+# model = pickle.load(open('model.pickle','rb'))
+p_in = open("model.pkl","rb")  # it needs class def, so dont' forget -> from naive_bayes_classifier import NB
+classifier = pickle.load(p_in)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -15,11 +19,8 @@ def index():
     pred = None
     form = reviewForm()
     if form.validate_on_submit():
-        review = form.review.data 
-
-        # predict the sentiment 
-
-        pred = review + " " + predict()  
+        review = form.review.data
+        pred = classifier.test(review)
     return render_template('home.html', form=form, pred=pred)
 
 # for handling errors -------------------------------
@@ -37,9 +38,6 @@ class reviewForm(FlaskForm):
     submit = SubmitField('Analyze')
 
 # ----------------------------------------------------
-def predict():
-    return "yes"
-
 
 
 
