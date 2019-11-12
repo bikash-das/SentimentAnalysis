f7fad64232763195e03b7e0851020e2b972540c9
Bikash Das <dasbikash989@gmail.com>
2019-11-12 10:18:38 +0530

    Added a Basic working POST API functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify,request,abort
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, TextAreaField
 from wtforms.validators import DataRequired
@@ -12,19 +12,20 @@ app.config['SECRET_KEY'] = '1234252333' # some secret key
 # model = pickle.load(open('model.pickle','rb'))
 # p_in = open("model.pkl","rb")  # it needs class def, so dont' forget -> from naive_bayes_classifier import NB
 # classifier = pickle.load(p_in)
+import pickle
+p_in = open("model.pkl","rb")  # it needs class def, so dont' forget -> from naive_bayes_classifier import NB
+classifier = pickle.load(p_in)
 
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route("/home", methods=['GET', 'POST'])
-def index():
-    import pickle
-    p_in = open("model.pkl","rb")  # it needs class def, so dont' forget -> from naive_bayes_classifier import NB
-    classifier = pickle.load(p_in)
+def index():   
     pred = None
     form = reviewForm()
     if form.validate_on_submit():
         review = form.review.data
-        pred = classifier.test(review) #use the classifier
+        pred = getSentiment(review)
+        # pred = classifier.test(review) #use the classifier
     return render_template('home.html', form=form, pred=pred)
 
 @app.route("/about")
@@ -35,6 +36,28 @@ def about():
 def api():
     return render_template("api.html")
 
+
+# ------------------------------ Basic API  ---------------------------
+@app.route("/api/v1.0/", methods=['POST'])
+def process():   
+    if not request.json:
+        abort(400)
+
+    r = request.json  # receive the data from client
+    polarity="error"
+
+    if len(r) == 0:
+        abort(500)  
+    if len(r) == 1:
+        polarity = getSentiment(r[0])
+    if len(r) > 1:
+        polarity = []
+        for review in r:
+            polarity.append(getSentiment(review))
+   
+    return jsonify({'result': polarity}), 201
+
+# ---------------------------Basic API Ends
 # for handling errors -------------------------------
 @app.errorhandler(404)
 def page_not_found(e):
@@ -50,9 +73,10 @@ class reviewForm(FlaskForm):
     submit = SubmitField('Analyze')
 
 # ----------------------------------------------------
-
+def getSentiment(review):
+    return classifier.test(review)
 
 
 if __name__ == '__main__':   
 
-    app.run(debug=1)
+    app.run(debug=1,port=5001)
